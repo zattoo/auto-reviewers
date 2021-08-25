24b665777a13b881a30708949cbf14cdea842601
Nitzan Nashi <nitzan.nashi@zattoo.com>
2021-08-25 15:38:45 +0200

    TTry

--- src/find-nearest-file.js
+++ src/find-nearest-file.js
@@ -26,6 +26,7 @@ const nextLevelUp = (directory) => {
  * @returns {string[]}
  */
 const findFiles = async (filename, directory, regex, foundFiles = []) => {
+    console.log(regex);
     const match = regex.exec(directory);
     console.log(`- file: ${directory}, match: ${match}`);
 
