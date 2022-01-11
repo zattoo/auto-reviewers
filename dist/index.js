0cc8497d4350cd0e713f3fc53b1f1e16acbe8353
Nitzan Nashi <nitzan.nashi@zattoo.com>
2022-01-11 14:47:24 +0100

    Handle linebreaks (#64)
    * TRY
    
    * doouble
    
    * build
    
    * CHANGELOG.md
    
    * CHANGELOG.md
    
    * FIX

--- dist/index.js
+++ dist/index.js
@@ -26880,7 +26880,7 @@ module.exports = {createRequiredApprovalsMap};
 const REVIEWERS_BLOCK_START = '<!-- reviewers start -->';
 const REVIEWERS_BLOCK_END = '<!-- reviewers end -->';
 
-const BLOCK_REGEX = new RegExp(`${REVIEWERS_BLOCK_START}(.|\r\n|\n)*${REVIEWERS_BLOCK_END}`);
+const BLOCK_REGEX = new RegExp(`(\\n)*(\\s)*${REVIEWERS_BLOCK_START}(.|\r\n|\n)*${REVIEWERS_BLOCK_END}(\\s)*(\\n)*`);
 
 /**
  * @param {string} body
@@ -26934,8 +26934,8 @@ const createTable = (requiredApprovalMap, length) => {
  * @returns {string}
  */
 const createCommentBlock = (owners, requiredApprovalMap) => {
-    if(!owners.length) {
-        return REVIEWERS_BLOCK_START + REVIEWERS_BLOCK_END;
+    if (!owners.length) {
+        return '';
     }
 
     const files = Object.keys(requiredApprovalMap);
@@ -26944,7 +26944,8 @@ const createCommentBlock = (owners, requiredApprovalMap) => {
     const table = createTable(requiredApprovalMap, filesLength);
 
     return (
-        REVIEWERS_BLOCK_START
+        '\n\n'
+        + REVIEWERS_BLOCK_START
         + '\n'
         + '## Reviewers'
         + '\n\n'
@@ -26952,7 +26953,7 @@ const createCommentBlock = (owners, requiredApprovalMap) => {
         + '\n'
         + table
         + REVIEWERS_BLOCK_END
-        + '\n'
+        + '\n\n'
     );
 };
 
@@ -26966,11 +26967,11 @@ const createUpdatedBody = (currentBody, owners, requiredApprovalMap) => {
     const body = currentBody || '';
     const comment = createCommentBlock(owners, requiredApprovalMap);
 
-    if(sameComment(body, comment)) {
+    if (sameComment(body, comment)) {
         return body;
     }
 
-    if(BLOCK_REGEX.test(body)) {
+    if (BLOCK_REGEX.test(body)) {
         return body.replace(BLOCK_REGEX, comment);
     }
 
